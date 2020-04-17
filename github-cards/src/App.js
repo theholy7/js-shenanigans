96562493d453f151a1e5482fed05f29ccc62197d
Jose Antunes <jose.antunes@splend.com>
2020-04-17 12:37:27 +0100

    formatting

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -28,24 +28,26 @@ class Card extends React.Component {
 
 class Form extends React.Component {
 	state = { userName: '' };
+	
 	handleSubmit = async (event) => {
-  	event.preventDefault();
-    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
-    this.props.onSubmit(resp.data);
-    this.setState({ userName: '' });
-  };
+  		event.preventDefault();
+    		const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+    		this.props.onSubmit(resp.data);
+    		this.setState({ userName: '' });
+  	};
+
 	render() {
   	return (
-    	<form onSubmit={this.handleSubmit}>
-    	  <input 
-          type="text" 
-          value={this.state.userName}
-          onChange={event => this.setState({ userName: event.target.value })}
-          placeholder="GitHub username" 
-          required 
-        />
-        <button>Add card</button>
-    	</form>
+		<form onSubmit={this.handleSubmit}>
+		  <input 
+		  type="text" 
+		  value={this.state.userName}
+		  onChange={event => this.setState({ userName: event.target.value })}
+		  placeholder="GitHub username" 
+		  required 
+		/>
+		<button>Add card</button>
+		</form>
     );
   }
 }
